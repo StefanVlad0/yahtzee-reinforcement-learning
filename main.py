753fb0e33d90cba911a986e5751f96cb74eeedca
Hutan Stefan <61848535+hutanstefan@users.noreply.github.com>
2024-10-16 21:34:23 +0300

    fix after player select

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from draw_module import draw_screen
+from draw_module import draw_screen,create_score_option_rects
 from event_module import handle_events
 import settings
 from settings import Messages
@@ -19,28 +19,6 @@ dice_images = [pygame.image.load(f"{i}.png") for i in range(1, 7)]
 clock = pygame.time.Clock()
 
 player = settings.Player()
-
-def create_score_option_rects():
-    table_width = 375
-    cell_height = 30
-    table_height = 510
-    x_start = settings.WIDTH - table_width - 20
-    y_start = settings.HEIGHT // 2 - table_height // 2
-
-    categories = [
-        "", "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes", "Sum", "Bonus",
-        "Three_of_a_kind", "Four_of_a_kind", "Full_House", "Small_straight",
-        "Large_straight", "Chance", "YAHTZEE", "TOTAL_SCORE"
-    ]
-
-    score_option_rects = []
-    for i, category in enumerate(categories):
-        if category:  # Sărim peste prima categorie goală
-            rect = pygame.Rect(x_start, y_start + i * cell_height, table_width // 3, cell_height)
-            score_option_rects.append((rect, category))
-
-    return score_option_rects
-
 
 roll_button = pygame.Rect(settings.WIDTH // 2 - 100, settings.HEIGHT - 300, 200, 50)
 selected_dices = []
@@ -80,6 +58,7 @@ while running:
     if endPlayerTurn:
         rolls_left = 3
         selected_dices = []
+        dice_values = []
         print(player.showScore())
 
     draw_screen(screen, dice_values, dice_images, roll_button, hover_button, clicked_button, selected_dices, button_disabled, message)
