cbd43b98927ad29d3873a459b9b0996b0eb3aefb
Vlad Stefan <vladysstefan@yahoo.com>
2025-01-04 16:48:30 +0200

    KR-NLP homework init

---
+++ KR-NLP-Homework/homework.py
@@ -0,0 +1,13 @@
+# Implement an application with the following functionalities:
+
+# - (0.25 points) Read a text from the command line or from a file.
+
+# - (0.25 points) Identify the language in which the text is written.
+
+# - (0.5 points) Display stylometric information (word and character length, word frequency, etc.) for that text (for example, using this guide: https://programminghistorian.org/en/lessons/introduction-to-stylometry-with-python).
+
+# - (1 point) Generate alternative versions of the text in which at least 20% of the words are replaced with synonyms (e.g., "house" - "home") and/or hypernyms (e.g., "house" - "building") and/or negated antonyms (e.g., "far" - "not near"). Use WordNet (https://wordnet.princeton.edu/ or https://www.racai.ro/p/llod/index_en.html for the lexical resource, and https://www.nltk.org/howto/wordnet.html for the API, for example).
+
+# - (1 point) Extract keywords from a paragraph (for example, using these methods: https://towardsdatascience.com/keyword-extraction-process-in-python-with-natural-language-processing-nlp-d769a9069d5c). Generate one sentence for each keyword found, preserving its meaning from the original text.
+
+# - Bonus (0.25 points): The initial text is in Romanian.
